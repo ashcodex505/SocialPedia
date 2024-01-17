cccae693d28cd5dac72820c4c53403b0fda786a8
ashcodex505 <ashishkurse@gmail.com>
2024-01-17 00:42:11 -0700

    We commited

--- server/index.js
+++ server/index.js
@@ -63,22 +63,17 @@ app.use("/posts", postRoutes);
 app.use("/search", searchRoutes);
 
 /*MONGOOSE SETUP*/
-mongoose.connect('mongodb+srv://ashishkurse:Retro101@cluster0.i4waxzt.mongodb.net/?retryWrites=true&w=majority');
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 
-app.listen(3001, () => {
-    console.log("Server is Running")
 })
-// const PORT = process.env.PORT || 6001;
-// mongoose.connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
+.then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-// })
-// .then(() => {
-//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-//     /* ADD DATA ONE TIME */  
-//     // User.insertMany(users);
-//     // Post.insertMany(posts);
-// })
-// .catch((error) => console.log(`${error} did not connect`));
+    /* ADD DATA ONE TIME */  
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+})
+.catch((error) => console.log(`${error} did not connect`));
